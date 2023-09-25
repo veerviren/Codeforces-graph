b50c0b48c70b085f7c2b3fe3e5a2fca63da09e30
viren.variya <viren.variya@celitotech.com>
2023-09-25 22:55:39 +0530

    inital graph

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import CalendarHeatmap from './CalendarHeatmap';
+import dummydata from './dummydata.json';
 
-function App() {
+const App = () => {
+  // Define initial values for selectedMonth and selectedYear
+  const [selectedMonth, setSelectedMonth] = useState('01'); // January
+  const [selectedYear, setSelectedYear] = useState('2019');
+
+  // Function to handle changes in the selected month
+  const handleMonthChange = (e) => {
+    setSelectedMonth(e.target.value);
+  };
+
+  // Function to handle changes in the selected year
+  const handleYearChange = (e) => {
+    setSelectedYear(e.target.value);
+  };
+
+  const constSize = {
+    width: '200px',
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Calendar Heatmap Example</h1>
+      <div>
+        <label>Select Month:</label>
+        <select value={selectedMonth} onChange={handleMonthChange}>
+          <option value="01">January</option>
+          <option value="02">February</option>
+          <option value="03">March</option>
+          <option value="04">April</option>
+          <option value="05">May</option>
+        </select>
+      </div>
+      <div>
+        <label>Select Year:</label>
+        <select value={selectedYear} onChange={handleYearChange}>
+          <option value="2023">2023</option>
+          <option value="2024">2024</option>
+          <option value="2019">2019</option>
+        </select>
+      </div>
+      <div>
+        <CalendarHeatmap
+          data={dummydata}
+          selectedMonth={selectedMonth}
+          selectedYear={selectedYear}
+          style={constSize}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/CalendarHeatmap.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import ReactCalendarHeatmap from 'react-calendar-heatmap';
+import 'react-calendar-heatmap/dist/styles.css';
+
+const CalendarHeatmap = ({ data, selectedMonth, selectedYear, style }) => {
+
+    const filteredData = data.filter((value) => {
+        const dateParts = value.date.split('-'); 
+        console.log("dateParts: ", dateParts);
+        const year = parseInt(dateParts[0], 10); 
+        const month = parseInt(dateParts[1], 10);
+        return year == selectedYear && month == selectedMonth;
+    });
+
+    const inlineCSS = `
+        .color-empty {
+            fill: white;
+        }
+        .color-scale-1 {
+            fill: #d6e685;
+        }
+        .color-scale-2 {
+            fill: #8cc665;
+        }
+        .color-scale-3 {
+            fill: #44a340;
+        }
+        .color-scale-4 {
+            fill: #1e6823;
+        }
+    `;
+    console.log("data: ", data);
+    console.log("selectedMonth: ", selectedMonth);
+    console.log("selectedYear: ", selectedYear);
+    console.log("filteredData: ", filteredData);
+    return (
+        <div className="calendar-heatmap" style={style}>
+            <style>{inlineCSS}</style> {/* Inline CSS */}
+            <ReactCalendarHeatmap
+                startDate={new Date(selectedYear, selectedMonth - 1, 1)}
+                endDate={new Date(selectedYear, selectedMonth, 0)}
+                values={filteredData}
+                classForValue={(value) => {
+                    if (!value) {
+                        return 'color-empty';
+                    }
+                    return `color-scale-${value.count < 4 ? value.count : 4}`;
+                }}
+                onClick={(value) => alert(`Clicked on value with count: ${value.count}`)}
+                showWeekdayLabels={true}
+            />
+        </div>
+    );
+};
+
+export default CalendarHeatmap;
