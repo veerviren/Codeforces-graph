e60d44ba19410175a07a1dff126a70f576ab9571
viren.variya <viren.variya@celitotech.com>
2023-09-30 02:48:03 +0530

    change graph dimenssion

--- src/CalendarHeatmap.js
+++ src/CalendarHeatmap.js
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import ReactCalendarHeatmap from 'react-calendar-heatmap';
 import 'react-calendar-heatmap/dist/styles.css';
 
@@ -8,10 +7,7 @@ const CalendarHeatmap = ({ data, selectedMonth, selectedYear }) => {
     const filteredData = data.filter((value) => {
         const dateParts = value.date.split('-'); 
         // console.log("dateParts: ", dateParts);
-        const year = parseInt(dateParts[0], 10); 
-        const month = parseInt(dateParts[1], 10);
-        // console.log("year: ", year);
-        // console.log("month: ", month);
+        const year = parseInt(dateParts[0], 10);
         return year == selectedYear;
     });
 
@@ -33,43 +29,31 @@ const CalendarHeatmap = ({ data, selectedMonth, selectedYear }) => {
         }
     `;
 
-    // let selectedDate = "";
-    // let selectedCount = "";
-
-    const [selectedDate, setSelectedDate] = useState("");
-    const [selectedCount, setSelectedCount] = useState("");
-
-    const testStyle = {   
-        "margin": "0px",
-        "padding": "0px",
-    };
     console.log("selectedMonth: ", selectedMonth);
     console.log("selectedYear: ", selectedYear);
     console.log("filteredData: ", filteredData);
     return (
-        <div className="calendar-heatmap">
-            <style>{inlineCSS}</style> 
-            <ReactCalendarHeatmap
-                startDate={new Date(`${selectedYear}-$01-01`)}
-                endDate={new Date(`${selectedYear}-$12-31`)}
-                values={filteredData}
-                classForValue={(value) => {
-                    if (!value) {
-                        return 'color-empty';
-                    }
-                    return `color-scale-${value.count < 4 ? value.count : 4}`;
-                }}
-                onClick={(value) => {
-                    if(value){
-                        setSelectedDate(value.date);
-                        setSelectedCount(value.count);
-                    }
-                }}
-                showWeekdayLabels={true}
-                showMonthLabels={true}
-                horizontal={true}
+        <div className="scrollable-container">
+            <div className="calendar-heatmap">
+                <style>{inlineCSS}</style>
+                <ReactCalendarHeatmap
+                    startDate={new Date(`${selectedYear}-$01-01`)}
+                    endDate={new Date(`${selectedYear}-$12-31`)}
+                    values={filteredData}
+                    classForValue={(value) => {
+                        if (!value) {
+                            return 'color-empty';
+                        }
+                        return `color-scale-${value.count < 4 ? value.count : 4}`;
+                    }}
+                    showOutOfRangeDays={true}   
+                    showWeekdayLabels={true}
+                    showMonthLabels={true}
+                    horizontal={true}
                 />
+            </div>
         </div>
+
     );
 };
 
